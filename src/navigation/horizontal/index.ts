ac9176b9fccde01b92bbfda77662cb9b84ab20e6
UhrinDavid <daviduhrin98@gmail.com>
2024-02-14 15:20:55 +0100

    Add all menu items

--- src/navigation/horizontal/index.ts
+++ src/navigation/horizontal/index.ts
@@ -6,6 +6,26 @@ const navigation = (t: any): HorizontalNavItemsType => [
     title: t('pageTitleCalendar'),
     path: '/calendar',
     icon: 'tabler:calendar'
+  },
+  {
+    title: t('pageTitleClients'),
+    path: '/clients',
+    icon: 'tabler:users'
+  },
+  {
+    title: t('pageTitleTrainings'),
+    path: '/trainings',
+    icon: 'tabler:run'
+  },
+  {
+    title: t('pageTitleStatistics'),
+    path: 'statistics',
+    icon: 'tabler:chart-bar'
+  },
+  {
+    title: t('pageTitleTasksAndGoals'),
+    path: '/task-and-goals',
+    icon: 'tabler:checkbox'
   }
 ]
 
